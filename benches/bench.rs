80a888c3d768d02e0bd3375cf5cc6d83b9bffc63
Bart Massey <bart.massey@gmail.com>
2022-02-03 07:57:42 -0800

    made more changes to bench organization

--- benches/bench.rs
+++ benches/bench.rs
@@ -8,17 +8,19 @@ use criterion::{
 use pow::*;
 
 macro_rules! bench_pow {
-    ($c: expr, $name:expr, $func:tt, $base:expr, $max_exp:expr) => {{
-        let id = format!("{}/{}", $name, $base);
-        let mut group = $c.benchmark_group(&id);
-        for exp in 0..=$max_exp {
-            group.bench_with_input(
-                BenchmarkId::new(&id, exp),
-                &exp,
-                |b, exp| b.iter(|| {
-                    black_box($func(black_box($base), black_box(*exp)))
-                }),
-            );
+    ($c: expr, $name:expr, $func:tt, $args:expr) => {{
+        let mut group = $c.benchmark_group($name);
+        for (base, max_exp) in $args {
+            let id = base.to_string();
+            for exp in 0..=*max_exp {
+                group.bench_with_input(
+                    BenchmarkId::new(&id, exp),
+                    &exp,
+                    |b, exp| b.iter(|| {
+                        black_box($func(black_box(*base), black_box(*exp)))
+                    }),
+                );
+            }
         }
         group.finish();
     }};
@@ -37,17 +39,15 @@ pub fn criterion_benchmark(c: &mut Criterion) {
         (7, 11),
         (8, 10),
     ];
-    for &(base, max_exp) in args {
-        bench_pow!(c, "pow_std", pow_std, base, max_exp);
-        bench_pow!(c, "pow_std_2opt", pow_std_2opt, base, max_exp);
-        bench_pow!(c, "pow_alt", pow_alt, base, max_exp);
-        bench_pow!(c, "pow_alt_2opt", pow_alt_2opt, base, max_exp);
-        bench_pow!(c, "pow_alt_01opt", pow_alt_01opt, base, max_exp);
-        bench_pow!(c, "pow_alt_012opt", pow_alt_012opt, base, max_exp);
-        bench_pow!(c, "u32::pow", (u32::pow), base, max_exp);
-        bench_pow!(c, "pow_alt_inline", pow_alt_inline, base, max_exp);
-        bench_pow!(c, "pow_alt_2opt_inline", pow_alt_2opt_inline, base, max_exp);
-    }
+    bench_pow!(c, "pow_std", pow_std, args);
+    bench_pow!(c, "pow_std_2opt", pow_std_2opt, args);
+    bench_pow!(c, "pow_alt", pow_alt, args);
+    bench_pow!(c, "pow_alt_2opt", pow_alt_2opt, args);
+    bench_pow!(c, "pow_alt_01opt", pow_alt_01opt, args);
+    bench_pow!(c, "pow_alt_012opt", pow_alt_012opt, args);
+    bench_pow!(c, "u32::pow", (u32::pow), args);
+    bench_pow!(c, "pow_alt_inline", pow_alt_inline, args);
+    bench_pow!(c, "pow_alt_2opt_inline", pow_alt_2opt_inline, args);
 }
 
 criterion_group!(benches, criterion_benchmark);
